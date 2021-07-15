89b02652e15c5dc396fe16b8a7d78fcec8774ac8
jaytai2040 <jayyr2040@gmail.com>
2021-07-15 20:27:39 +0800

    set up server.js and index route

--- server.js
+++ server.js
@@ -4,10 +4,32 @@ touch server.js .env .gitignore
 mkdir views models
 touch views/index.ejs views/show.ejs 
 npm init -y
-npm i express dotenv ejs
+npm i express dotenv ejs method-override
 npm install -D nodemon
 code .
 in .gitignore .env and node_modules/ 
 in .env PORT=3000
-in server.js
+in server.js set up file, in package.json, edit "start": "nodemon server.js"
 */
+
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT;
+const methodOverride = require("method-override");
+const pokedex = require("./models/pokemon");
+
+app.use(express.urlencoded({ extended: false })); 
+app.use(express.static('public'));
+app.use(methodOverride("_method"));
+
+
+app.get("/pokemon", (req, res) => {
+    res.send("Pokdex route working!");
+  });
+
+  
+app.listen(PORT, () => {
+    console.log("Pokedex Up and Running on port: ", PORT);
+  });
+  
