eab1575fb8e658aab58b91105f18ef421c42c777
jaytai2040 <jayyr2040@gmail.com>
2021-07-15 20:18:35 +0800

    Settting up files and folder

---
+++ server.js
@@ -0,0 +1,13 @@
+/*
+mkdir, cd into the dir
+touch server.js .env .gitignore
+mkdir views models
+touch views/index.ejs views/show.ejs 
+npm init -y
+npm i express dotenv ejs
+npm install -D nodemon
+code .
+in .gitignore .env and node_modules/ 
+in .env PORT=3000
+in server.js
+*/
